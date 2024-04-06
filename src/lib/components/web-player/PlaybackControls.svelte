6df8d5fa2658c77ad188fba45cb97b4a031aa808
Navindu Amarakoon <navanavi3@gmail.com>
2024-04-06 11:32:30 +0530

    feat: :sparkles: playback controls

---
+++ src/lib/components/web-player/PlaybackControls.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { spotifyPlaybackState, spotifyPlayer } from '$lib/stores/spotify.store';
+
+	const handlePrevPress = () => {
+		$spotifyPlayer?.previousTrack();
+	};
+
+	const handleNextPress = () => {
+		$spotifyPlayer?.nextTrack();
+	};
+
+	const handlePlayPress = () => {
+		$spotifyPlayer?.togglePlay();
+	};
+</script>
+
+<div class="flex gap-2 mt-3">
+	<button on:click={handlePrevPress} class="w-6 h-6 bg-white/40 flex items-center justify-center">
+		&lt;&lt;
+	</button>
+
+	<button on:click={handlePlayPress} class="w-6 h-6 bg-white/40 flex items-center justify-center">
+		{#if $spotifyPlaybackState?.paused}
+			p
+		{:else}
+			||
+		{/if}
+	</button>
+	<button on:click={handleNextPress} class="w-6 h-6 bg-white/40 flex items-center justify-center">
+		&gt;&gt;
+	</button>
+</div>
