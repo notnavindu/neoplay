fce4d1b8de82d45f3053916d65fdaaaa5608e1d8
Navindu Amarakoon <navanavi3@gmail.com>
2024-04-13 08:42:32 +0530

    feat: :lipstick: sidebar WIP

--- src/routes/main/+layout.svelte
+++ src/routes/main/+layout.svelte
@@ -6,6 +6,7 @@
 	import WebPlayerListener from '$lib/components/listeners/WebPlayerListener.svelte';
 	import { auth } from '$lib/stores/auth.store';
 	import { spotifyPlayerReady } from '$lib/stores/spotify.store';
+	import { leftSidebarOpen } from '$lib/stores/ui.store';
 
 	if (!$auth.isLoggedIn) goto('/');
 </script>
@@ -14,7 +15,16 @@
 <WebPlayerListener />
 
 {#if $spotifyPlayerReady}
-	<Sidebar />
+	{#if $leftSidebarOpen}
+		<div>
+			<Sidebar />
+		</div>
+	{/if}
+
+	<button
+		class="absolute top-4 bg-neo-blue p-2 z-40"
+		on:click={() => ($leftSidebarOpen = !$leftSidebarOpen)}>OPEN</button
+	>
 
 	<div class="w-full flex flex-col justify-between">
 		<div class="p-4 relative">
